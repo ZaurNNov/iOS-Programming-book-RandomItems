5c60e10a4ba372b579cdd81f5b25c6f89bcb7ccf
Zaur Giyasov <zagiyas@mera.ru>
2018-04-17 14:49:00 +0300

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Zaur Giyasov on 17/04/2018.
+//  Copyright © 2018 Zaur Giyasov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
